937412f3fa8e0eb0851965cef0a1b6f79d458308
yyamada12 <12yacropolisy@gmail.com>
2021-02-17 21:31:31 +0900

    initial commit

---
+++ src/test/java/mrs/mrs/MrsApplicationTests.java
@@ -0,0 +1,11 @@
+package mrs.mrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class MrsApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
