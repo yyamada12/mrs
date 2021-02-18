36bb94393b16af3427c6975020a645c01c892b83
yyamada12 <12yacropolisy@gmail.com>
2021-02-18 22:54:31 +0900

    add entity

---
+++ src/main/java/mrs/domain/model/ReservableRoomId.java
@@ -0,0 +1,45 @@
+package mrs.domain.model;
+
+import javax.persistence.Embeddable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Embeddable
+public class ReservableRoomId {
+    private Integer roomId;
+
+    private LocalDate reservedDate;
+
+    public ReservableRoomId(Integer roomId, LocalDate reservedDate) {
+        this.roomId = roomId;
+        this.reservedDate = reservedDate;
+    }
+
+    public ReservableRoomId() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ReservableRoomId that = (ReservableRoomId) o;
+        return Objects.equals(roomId, that.roomId) && Objects.equals(reservedDate,
+                that.reservedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomId, reservedDate);
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+}
