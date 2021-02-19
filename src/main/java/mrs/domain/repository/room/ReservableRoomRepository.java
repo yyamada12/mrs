db49a46184b7b761cb6c698a4035ea54048488a5
yyamada12 <12yacropolisy@gmail.com>
2021-02-19 13:52:56 +0900

    add room view, controller, repository

---
+++ src/main/java/mrs/domain/repository/room/ReservableRoomRepository.java
@@ -0,0 +1,13 @@
+package mrs.domain.repository.room;
+
+import mrs.domain.model.ReservableRoom;
+import mrs.domain.model.ReservableRoomId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, ReservableRoomId> {
+    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(
+            LocalDate reservedDate);
+}
