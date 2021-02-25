3f88a04c085b9ddf74f786b89715a1ed24e6d887
yyamada12 <12yacropolisy@gmail.com>
2021-02-25 23:04:45 +0900

    resolve N+1 query

--- src/main/java/mrs/domain/repository/room/ReservableRoomRepository.java
+++ src/main/java/mrs/domain/repository/room/ReservableRoomRepository.java
@@ -4,6 +4,8 @@ import mrs.domain.model.ReservableRoom;
 import mrs.domain.model.ReservableRoomId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.time.LocalDate;
@@ -14,6 +16,6 @@ public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     ReservableRoom findOneForUpdateByReservableRoomId(ReservableRoomId reservableRoomId);
 
-    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(
-            LocalDate reservedDate);
+    @Query("SELECT DISTINCT x FROM ReservableRoom x LEFT JOIN FETCH x.meetingRoom WHERE x.reservableRoomId.reservedDate = :date ORDER BY x.reservableRoomId.roomId ASC")
+    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(@Param("date") LocalDate reservedDate);
 }
